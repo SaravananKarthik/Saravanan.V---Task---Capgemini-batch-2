7a74996dadee1f5b603113cd9b810ce2666eff52
Saravanan V <90813730+SaravananKarthik@users.noreply.github.com>
2021-11-16 00:31:26 -0800

    Add files via upload
    example program in opreators

---
+++ opreators.java
@@ -0,0 +1,26 @@
+package saravanan;
+
+public class opreators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 12, b = 5;
+
+		    // addition operator
+		    System.out.println("a + b = " + (a + b));
+
+		    // subtraction operator
+		    System.out.println("a - b = " + (a - b));
+
+		    // multiplication operator
+		    System.out.println("a * b = " + (a * b));
+
+		    // division operator
+		    System.out.println("a / b = " + (a / b));
+
+		    // modulo operator
+		    System.out.println("a % b = " + (a % b));
+
+	}
+
+}
