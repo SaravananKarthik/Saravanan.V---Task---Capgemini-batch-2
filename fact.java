e7f73fd09fd7da3f786bae417c6be394476f8934
Saravanan V <90813730+SaravananKarthik@users.noreply.github.com>
2021-11-16 03:30:38 -0800

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,21 @@
+package saravanan;
+
+public class fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int n=5;
+			
+			int fact =1, i;
+			
+			for(i=1; i<=n; i++)
+			{
+				fact= fact * i;
+				
+			}
+			System.out.println(fact);
+
+	}
+
+}
